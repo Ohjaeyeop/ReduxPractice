56ce6658cb4d91299be9609e87bc3f9e63dbf4c7
OhJaeyeop <ohbcj0831@gmail.com>
2022-01-12 16:32:29 +0900

    마이페이지 헤더

--- src/features/users/MyPostsList.tsx
+++ src/features/users/MyPostsList.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {Button, Text, View} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {MyPostsListProps} from '../../App';
+import {useUser} from '../../contexts/userContext';
 
 const MyPostsList = ({navigation}: MyPostsListProps) => {
+  const {user} = useUser();
   const signOut = () => {
     auth()
       .signOut()
@@ -11,9 +13,22 @@ const MyPostsList = ({navigation}: MyPostsListProps) => {
         navigation.navigate('PostsList');
       });
   };
+  const displayName = user.email.split('@')[0];
   return (
     <View>
-      <Button title="Sign Out" color="#774ABC" onPress={signOut} />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: 15,
+        }}>
+        <Text style={{fontSize: 18, color: '#774ABC', paddingTop: 8}}>
+          {displayName} 님
+        </Text>
+        <View>
+          <Button title="Sign Out" color="#774ABC" onPress={signOut} />
+        </View>
+      </View>
     </View>
   );
 };
